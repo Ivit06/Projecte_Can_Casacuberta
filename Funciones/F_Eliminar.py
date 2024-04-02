8ef8895e573be712a3e76c266b1eaaf6eadcf3b8
Asix1 <asix1@Portatil.asix1.org>
2024-04-02 20:18:00 +0200

    FUnción eliminar funcionando correctamente

--- Funciones/F_Eliminar.py
+++ Funciones/F_Eliminar.py
@@ -1,20 +1,19 @@
 def eliminar_titulo (archivo):
     try:
-        titulo_libro = input("Introduce el título del libro que quieres eliminar: ")
+        dato = input("Introduce el Títol o Autor/a o Any de publicació o Genere o ISBN del libro/s que quieres eliminar: ") #Si pones un genero hay que tener cuidado porque borra todos los de ese genero
         with open(archivo, 'r') as a:
             contenido = a.readlines()
         with open(archivo, 'w') as a:
             for linea in contenido:
-                if linea.strip("\n") != titulo_libro:
+                if dato not in linea:
                     a.write(linea)
     except FileNotFoundError:
         print("El archivo 'archivo.txt' no se encontró.")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
     else:
-        print(f"El libro con el título {titulo_libro} ha sido eliminado de la lista de libros.")
-        print (contenido)
-
+        print(f"El libro con este dato {dato} ha sido eliminado de la lista de libros.")
+        a.close()
 
 archivo2="Llibres.txt"
 eliminar_titulo(archivo2)
